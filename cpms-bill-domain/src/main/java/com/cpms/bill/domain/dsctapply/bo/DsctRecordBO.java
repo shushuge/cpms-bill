714cdba7af89c10dd3017ccb9b1276ce3edcc349
zengsheng <593683869@qq.com>
2024-02-20 18:03:49 +0800

    聚合根

---
+++ cpms-bill-domain/src/main/java/com/cpms/bill/domain/dsctapply/bo/DsctRecordBO.java
@@ -0,0 +1,21 @@
+package com.cpms.bill.domain.dsctapply.bo;
+
+import com.cpms.bill.domain.dsctapply.pojo.po.DsctRecord;
+import com.cpms.bill.domain.dsctapply.pojo.po.DsctRecordItem;
+
+import java.util.List;
+
+// 贴现申请聚合根（Aggregate Root）
+public class DsctRecordBO {
+    private DsctRecord dsctRecord;
+    private List<DsctRecordItem> itemList;
+
+
+    public void addItem(){
+
+    }
+
+    public void deleteItem(){
+
+    }
+}
